52a233c05033a570f93d93f1c901e07a09cba357
Daniel-KI <dankorshov@gmail.com>
2021-01-22 00:29:13 +0300

    eat: realise min(), max(), avg() functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(isEmptyArr(array)) return 0;
+
+  var minElement = 0;
+  array.forEach(element => {
+    if(element < minElement) minElement = element;
+  });
+  return minElement;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(isEmptyArr(array)) return 0;
+
+  var maxElement = 0;
+  array.forEach(element => {
+    if(element > maxElement) maxElement = element;
+  });
+  return maxElement;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(isEmptyArr(array)) return 0;
+
+  var sum = 0;
+  array.forEach(element => {
+    sum += element;
+  });
+  return sum / array.length;
+}
+
+function isEmptyArr(array){
+  return typeof array != "undefined" && array != null && array.length > 0 ? false : true;
 }
